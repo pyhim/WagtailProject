cfc71f7287cbe65f88313e3d932040f8dbc207d2
Kristinabuts <bucyara007@gmail.com>
2021-08-21 13:12:58 +0300

    creating git

---
+++ core/wagsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-1(9$g3cs&nieuv5oq21#w^v9zj%wa1d4tb5)ia5sol#g4iynlc'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
